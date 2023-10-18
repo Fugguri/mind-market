fe7e751001df1ce0c0469172f62a907c455eeec6
Fugguri <fygguri@icloud.com>
2023-10-19 02:26:00 +0300

    update

---
+++ app/(main)/(routes)/integrations/page.tsx
@@ -0,0 +1,11 @@
+const IntegrationsPage = () => {
+    return ( <div className="flex items-center align-middle font-roboto">
+        <div>
+            <h1>
+                Этот раздел в разработке ...
+            </h1>
+        </div>
+    </div> );
+}
+ 
+export default IntegrationsPage;
