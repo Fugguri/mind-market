41f2a452d151406eeea19cf23b2e8026bed8b76a
Fugguri <fygguri@gmail.com>
2023-10-24 23:21:26 +0300

    update

--- app/(main)/(routes)/assistants/layout.tsx
+++ app/(main)/(routes)/assistants/layout.tsx
@@ -1,39 +1,38 @@
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation'
 
-import { db } from "@/lib/db";
-import { currentProfile } from "@/lib/current-profile";
-import AssistantSidebar from "@/components/assistant/assistant-sidebar";
-import { signIn } from "next-auth/react";
+import { db } from '@/lib/db'
+import { currentProfile } from '@/lib/current-profile'
+import AssistantSidebar from '@/components/assistant/assistant-sidebar'
+import { signIn } from 'next-auth/react'
 
 const AssistantPageLayuot = async ({
-    children,
-
-}: { children: React.ReactNode,
+	children,
+}: {
+	children: React.ReactNode
 }) => {
-    const profile = await currentProfile();
+	const profile = await currentProfile()
 
-    if (!profile) {
-        return signIn
-    };
-    const assistant = await db.assistant.findMany({
-        where: {
-            profileId: profile.id
-        }
-    })
-    if (!assistant) {
-        return redirect("/")
-    }
-    return (  
-        
-        <main className="h-full md:pl-60">
-        <div className="h-full">
-        <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-            <AssistantSidebar/>
-        </div>
-            {children}
-        </div>
-        </main>
-  )  ;
+	if (!profile) {
+		return signIn
+	}
+	const assistant = await db.assistant.findMany({
+		where: {
+			profileId: profile.id,
+		},
+	})
+	if (!assistant) {
+		return redirect('/')
+	}
+	return (
+		<main className='h-full'>
+			<div className='h-full'>
+				<div className='hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0'>
+					<AssistantSidebar />
+				</div>
+				<div className=' md:pl-60'>{children}</div>
+			</div>
+		</main>
+	)
 }
- 
-export default AssistantPageLayuot;
+
+export default AssistantPageLayuot
---
+++ components/mics/in-dev-banner.tsx
@@ -0,0 +1,9 @@
+const InDevBanner = () => {
+	return (
+		<div className=''>
+			<p>Страница в разработке...</p>
+		</div>
+	)
+}
+
+export default InDevBanner
