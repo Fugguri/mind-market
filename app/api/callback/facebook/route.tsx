e46726cd9e789e49912d31dafa1b285152c447ac
Fugguri <fygguri@gmail.com>
2023-10-25 00:15:46 +0300

    update

---
+++ app/api/callback/facebook/route.tsx
@@ -0,0 +1,21 @@
+// import axios from 'axios'
+
+// import { NextResponse } from 'next/server'
+
+import { db } from '@/lib/db'
+
+export async function POST(req: Request) {
+	console.log(req)
+}
+
+export async function GET(req: Request) {
+	console.log(req)
+}
+
+export async function PATCH(req: Request) {
+	console.log(req)
+}
+
+export async function DELETE(req: Request) {
+	console.log(req)
+}
