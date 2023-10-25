c614658501efa95f2fd91047cf03ee6775fcb12b
Fugguri <fygguri@gmail.com>
2023-10-25 04:07:06 +0300

    update

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,35 +1,35 @@
-"use client"
+'use client'
 
-import { CreateAssistantModal } from "@/components/modals/create-assistant-modal"
-import { useEffect, useState } from "react"
-import { AddTgBotModal } from "@/components/modals/integrations/tg-bot-modal";
-import { AddTgUserBotModal } from "@/components/modals/integrations/tg-user-bot-modal";
-import { AddWaBotModal } from "@/components/modals/integrations/wa-bot-modal";
-import { DeleteAssistantModal } from "@/components/modals/delete-assistant-modal";
-import { EditAssistantModal } from "@/components/modals/edit-assistant-modal";
+import { CreateAssistantModal } from '@/components/modals/create-assistant-modal'
+import { useEffect, useState } from 'react'
+import { AddTgBotModal } from '@/components/modals/integrations/tg-bot-modal'
+import { AddTgUserBotModal } from '@/components/modals/integrations/tg-user-bot-modal'
+import { AddWaBotModal } from '@/components/modals/integrations/wa-bot-modal'
+import { DeleteAssistantModal } from '@/components/modals/delete-assistant-modal'
+import { EditAssistantModal } from '@/components/modals/edit-assistant-modal'
+import { AddInstModal } from '../modals/integrations/instagram-modal'
 // import { CreateIntegrationModal } from "@/components/modals/create-integration-modal";
 
 export const ModalProvider = () => {
+	const [isMounted, setIsMounted] = useState(false)
 
-    const [isMounted, setIsMounted] = useState(false);
+	useEffect(() => {
+		setIsMounted(true)
+	}, [])
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, [])
-    
-    if(!isMounted) {
-        return null;
-    }
+	if (!isMounted) {
+		return null
+	}
 
-    return(
-        <>
-        <AddTgBotModal/>
-        <AddTgUserBotModal/>
-        <AddWaBotModal/>
-        <CreateAssistantModal/>
-        <DeleteAssistantModal/>
-        <EditAssistantModal/>
-        
-        </>
-    )
+	return (
+		<>
+			<AddTgBotModal />
+			<AddTgUserBotModal />
+			<AddInstModal />
+			<AddWaBotModal />
+			<CreateAssistantModal />
+			<DeleteAssistantModal />
+			<EditAssistantModal />
+		</>
+	)
 }
