77f9177b69bcfbf8c03af6cc4a5b90d4656ef66a
Fugguri <fygguri@gmail.com>
2023-10-28 06:28:49 +0300

    update

--- middleware.tsx
+++ middleware.tsx
@@ -1,3 +1,5 @@
 export { default } from 'next-auth/middleware'
 
-export const config = { matcher: ['/profile', '/settings/:path*'] }
+export const config = {
+	matcher: ['/profile', '/settings/:path*', '/profile/:path*', '/chats/:path*'],
+}
