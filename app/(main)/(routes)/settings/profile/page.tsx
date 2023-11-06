acdb38965ac858ef7c2af2b64a36af62c4c8c638
Fugguri <fygguri@gmail.com>
2023-11-06 16:07:41 +0300

    update

--- app/(main)/(routes)/settings/profile/page.tsx
+++ app/(main)/(routes)/settings/profile/page.tsx
@@ -1,18 +1,12 @@
 import { currentProfile } from '@/lib/current-profile'
-import { db } from '@/lib/db'
 import { redirect } from 'next/navigation'
-
-import { useModal } from '@/hooks/use-modal-store'
 import PageTitleItem from '@/components/ui/page_title'
-import Image from 'next/image'
-import { Button } from '@/components/ui/button'
-import { Edit } from 'lucide-react'
 import ProfileItemPage from '@/components/profile/profile-item'
 
 const ProfilePage = async () => {
 	const profile = await currentProfile()
 	if (!profile) {
-		// redirectToSignIn()
+		redirect('/')
 	}
 	return (
 		<div className='m-5 '>
