e1348608c4666832913933f2805a141f03786524
Fugguri <fygguri@gmail.com>
2023-11-07 10:38:55 +0300

    update

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+	return <p>MindMarket</p>
+}
+
+export default HomePage
--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,16 +2,17 @@ import { db } from '@/lib/db'
 import { redirect } from 'next/navigation'
 import { initialProfile } from '@/lib/initial-profile'
 import { InitialModal } from '@/components/modals/initial-modal'
+import MobileHomePage from '@/components/full_size/mobile-home-page'
 // import FullScreenHomePage from '@/components/full_sie/full-screen-home-page'
 // import MobileHomePage from '@/components/full_sie/mobile-home-page'
 
 const SetupPage = async () => {
 	const profile = await initialProfile()
-	return redirect(`chats/inbox`)
-	// return (
-	// 	// <FullScreenHomePage />
-	// 	<MobileHomePage />
-	// )
+	// return redirect(`chats/inbox`)
+	return (
+		// <FullScreenHomePage />
+		<MobileHomePage />
+	)
 }
 
 export default SetupPage
