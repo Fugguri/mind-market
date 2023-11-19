f9656859008c20e484e514efca374eb08213bf54
Fugguri <fygguri@gmail.com>
2023-11-19 21:22:00 +0300

    update

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from '@/lib/initial-profile'
 import MobileHomePage from '@/components/full_size/mobile-home-page'
 import FullScreenHomePage from '@/components/full_size/full-screen-home-page'
+import { redirect } from 'next/navigation'
 // import FullScreenHomePage from '@/components/full_sie/full-screen-home-page'
 // import MobileHomePage from '@/components/full_sie/mobile-home-page'
 
