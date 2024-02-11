fec7f077337d5c34ffd8ec22fa4da0437b9284f2
Fugguri <fygguri@gmail.com>
2024-02-11 21:18:51 +0300

    update

--- config/auth.ts
+++ config/auth.ts
@@ -7,20 +7,18 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import { db } from '@/lib/db'
 import { randomUUID } from 'crypto'
 import { hash, compare } from 'bcrypt'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
 export const authConfig: AuthOptions = {
 	// Configure one or more authentication providers
 
 	session: {
 		strategy: 'jwt',
 	},
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		CredentialsProvider({
-			// The name to display on the sign in form (e.g. "Sign in with...")
 			name: 'email',
-			// `credentials` is used to generate a form on the sign in page.
-			// You can specify which fields should be submitted, by adding keys to the `credentials` object.
-			// e.g. domain, username, password, 2FA token, etc.
-			// You can pass any HTML attribute to the <input> tag through the object.
 			credentials: {
 				email: {
 					label: ' email ',
