9d62a3d226c8b8f644c69ccfc6d0e64aa0e60a90
Fugguri <fygguri@gmail.com>
2024-03-03 18:06:50 +0300

    update

--- app/(main)/(routes)/projects/page.tsx
+++ app/(main)/(routes)/projects/page.tsx
@@ -1,15 +1,41 @@
-import { currentProfile } from "@/lib/current-profile";
-import { redirectToSignIn } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+import { currentProfile } from '@/lib/current-profile'
+import { redirectToSignIn } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import ChatItem from '@/components/chats/chat-item'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Separator } from '@/components/ui/separator'
+const ProjectsPage = async () => {
+	const profile = await currentProfile()
 
-const ChatPage = async () => {
-    const profile = await currentProfile()
+	// if(!profile) {
+	//     redirectToSignIn()
+	// }
+	const chats = [
+		{
+			id: '54523',
+			text: 'some ',
+			title: 'some d',
+			time: 'some da',
+			channel: 'some da',
+		},
+		{
+			id: '5452rew3',
+			text: 'some ',
+			title: 'some d',
+			time: 'some da',
+			channel: 'some da',
+		},
+	]
 
-    // if(!profile) {
-    //     redirectToSignIn()
-    // }
-
-    return ( redirect("") );
+	return (
+		<div>
+			<Separator orientation='horizontal' />
+			<div className='grid grid-cols-3 m-5 h-3 align-top '>
+				<div className='col-span-1 flex align-top font-size-12'>Сообщение</div>
+				<div className='col-span-1 flex items-center font-size-12'>Время </div>
+				<div className='col-span-1 flex items-center font-size-12'>Канал </div>
+			</div>
+		</div>
+	)
 }
- 
-export default ChatPage;
+export default ProjectsPage
