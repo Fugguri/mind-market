b2936e0bf4907f753cf49feef7dd71163491f5a4
Fugguri <fygguri@gmail.com>
2024-03-05 17:53:27 +0300

    update

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -4,12 +4,14 @@ import FullScreenHomePage from '@/components/full_size/full-screen-home-page'
 import { redirect } from 'next/navigation'
 import { currentProfile } from '@/lib/current-profile'
 import { signIn } from 'next-auth/react'
+import { LogOutButton } from '@/components/auth'
 // import FullScreenHomePage from '@/components/full_sie/full-screen-home-page'
 // import MobileHomePage from '@/components/full_sie/mobile-home-page'
 
 const SetupPage = async () => {
 	const profile = await currentProfile()
 	if (!profile) {
+		return <LogOutButton />
 		signIn()
 		// return redirect('/api/auth/singing')
 	}
