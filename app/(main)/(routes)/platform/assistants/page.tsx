063750568f2a0799269df3dbb3f7a8a2d86aa141
Fugguri <fygguri@gmail.com>
2024-05-30 15:52:08 +0300

    update

--- app/(main)/(routes)/platform/assistants/page.tsx
+++ app/(main)/(routes)/platform/assistants/page.tsx
@@ -8,20 +8,41 @@ import { BtnFilledIcon, BtnOutlineIcon } from "@/components/buttons";
 
 import { ScrollShadow } from "@nextui-org/react";
 import { useModal } from "@/hooks/use-modal-store";
+import { currentProfile } from "@/lib/current-profile";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-export default function Page() {
+const AssistantsPage = async () => {
   const { onOpen } = useModal()
+	const profile = await currentProfile()
+	if (!profile) {
+		return redirect('/api/auth/signin')
+	}
 
+  const project = await db.project.findFirst({
+
+    where: {
+      ownerId: profile?.id,
+		},
+  }
+  )
   // get assistants logic
-  const assistants = null
+  const assistants = await db.assistant.findMany({where:{
+    projectId:project?.id
+  }})
 
   return (
     <>
       <Pane height="100%" width="100%" direction="column" justify="unset">
         <PaneTitle>Список ассистентов</PaneTitle>
         <ScrollShadow size={15} className="flex flex-row flex-wrap overflow-y-scroll h-[100%] gap-[2rem]">
-          {/* API */}
-          <AssistantCard name="Test" platform={Platforms.vk}/>
+          {assistants.map((assistant)=> {
+
+            <AssistantCard name={assistant.name} platform={assistant.use_count}/>
+          }
+
+          ) }
+          
         </ScrollShadow>
         <div className="flex flex-row content-end h-[3rem] w-1/2 gap-[1rem]">
           {/* API */}
@@ -32,3 +53,5 @@ export default function Page() {
     </>
   );
 }
+
+export default AssistantsPage;
