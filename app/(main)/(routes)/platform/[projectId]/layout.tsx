6964c9b8c45cd2310e71f2d66e8e0269f6edd273
Fugguri <fygguri@gmail.com>
2024-05-30 17:55:54 +0300

    update

--- app/(main)/(routes)/platform/[projectId]/layout.tsx
+++ app/(main)/(routes)/platform/[projectId]/layout.tsx
@@ -1,66 +1,28 @@
 // import * as React from "react";
-"use client"
 import { SideNav } from "@/components/crm/sidenav/sidenav";
-import SideBtn from "@/components/crm/sidenav/sidenav-btn";
 import "./globals.css";
 import "@/styles/global.css";
-import { PanesContainer } from "@/components/crm/panes";
+
 import Header from "@/components/crm/header";
 import {
   CardShellDefaultLink,
   CardModuleDescription,
 } from "@/components/cards";
-import Link from "next/link";
-import { useParams } from "next/navigation";
+import SidebarButtons from "@/components/mics/sidebar-buttons";
 
 export const metadata = {
   title: "CRM",
 };
 
 export default function CRMLayout({ children }: { children: React.ReactNode }) {
-	const params = useParams()
 
   return (
     <>
       <div className="screenWrapper">
         <SideNav>
-          <div className="flex flex-col gap-[1rem]">
-          <Link href="/platform">
-    
-            <div
-              className="logo"
-              style={{
-                  display: "block",
-                  height: "3rem",
-                  width: "12rem",
-                margin: "1rem 0",
-                backgroundSize: "12rem",
-                backgroundClip: "content-box",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundImage: "var(--fullLogo)",
-            }}
-            />
-            </Link>
-            <CardShellDefaultLink link="" maxWidth="10rem">
-              <CardModuleDescription>
-                Динамическое планирование
-              </CardModuleDescription>
-            </CardShellDefaultLink>
-            <div className="divider transparent" id="smallest" />
-            <SideBtn icon="account_circle" text="Профиль" link={`/platform/${params.projectId}profile`} />
-            <SideBtn icon="mail" text="Сообщения" link={`/platform/${params.projectId}chat`} />
-            <SideBtn
-              icon="leaderboard"
-              text="Статистика" 
-              link={`/platform/${params.projectId}dashboard`}
-            />
-            <SideBtn icon="group" text="Ассистенты" link={`/platform/${params.projectId}assistants`} />
-            <SideBtn icon="credit_card" text="Оплата" link={`/platform/${params.projectId}billing`} />
-            <SideBtn icon="call" text="Звонки" link={`/platform/${params.projectId}call`} />
-            <SideBtn icon="stacks" text="Интеграции" link={`/platform/${params.projectId}integrations`} />
-            <SideBtn icon="description" text="CRM" link={`/platform/${params.projectId}crm`} />
-          </div>
+
+        <SidebarButtons/>
+
           <div className="opacity-50 mb-[2rem]">
             <CardShellDefaultLink link="/platform" maxWidth="10rem">
               <CardModuleDescription>На главную</CardModuleDescription>
---
+++ components/mics/sidebar-buttons.tsx
@@ -0,0 +1,56 @@
+"use client"
+import SideBtn from "@/components/crm/sidenav/sidenav-btn";
+import Link from "next/link";
+import { PanesContainer } from "@/components/crm/panes";
+import { useParams } from "next/navigation";
+import {
+    CardShellDefaultLink,
+    CardModuleDescription,
+  } from "@/components/cards";
+
+const SidebarButtons = () => {
+
+	const params = useParams()
+
+    return ( 
+        <div className="flex flex-col gap-[1rem]">
+        <Link href="/platform">
+  
+          <div
+            className="logo"
+            style={{
+                display: "block",
+                height: "3rem",
+                width: "12rem",
+              margin: "1rem 0",
+              backgroundSize: "12rem",
+              backgroundClip: "content-box",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              backgroundImage: "var(--fullLogo)",
+          }}
+          />
+          </Link>
+          <CardShellDefaultLink link="" maxWidth="10rem">
+            <CardModuleDescription>
+              Динамическое планирование
+            </CardModuleDescription>
+          </CardShellDefaultLink>
+        <div className="divider transparent" id="smallest" />
+        <SideBtn icon="account_circle" text="Профиль" link={`/platform/${params.projectId}profile`} />
+        <SideBtn icon="mail" text="Сообщения" link={`/platform/${params.projectId}chat`} />
+        <SideBtn
+          icon="leaderboard"
+          text="Статистика" 
+          link={`/platform/${params.projectId}dashboard`}
+        />
+        <SideBtn icon="group" text="Ассистенты" link={`/platform/${params.projectId}assistants`} />
+        <SideBtn icon="credit_card" text="Оплата" link={`/platform/${params.projectId}billing`} />
+        <SideBtn icon="call" text="Звонки" link={`/platform/${params.projectId}call`} />
+        <SideBtn icon="stacks" text="Интеграции" link={`/platform/${params.projectId}integrations`} />
+        <SideBtn icon="description" text="CRM" link={`/platform/${params.projectId}crm`} />
+      </div>
+     );
+}
+ 
+export default SidebarButtons;
