cccb3b6d510677abb72110149799b63d3de5624c
Fugguri <fygguri@gmail.com>
2024-06-04 17:51:45 +0300

    update

--- components/modals/assistants/add-assistants-selection-modal.tsx
+++ components/modals/assistants/add-assistants-selection-modal.tsx
@@ -7,7 +7,7 @@ import { BtnFilledIcon } from "../../buttons"
 
 const AddASsistantsSelectionModal = () => {
 	const params = useParams()
-
+	const router = useRouter() 
 	const { isOpen, onClose, type, data ,onOpen} = useModal()
 	const isModalOpen = isOpen && type === 'addAssistansSelection'
 
@@ -28,10 +28,11 @@ const AddASsistantsSelectionModal = () => {
 						<DialogDescription className='tetx-center text-zinc-500'>
 						<div  className=' items-center align-middle font-roboto ml-15'>
 							<div className=' m-10 p-10 align-middle items-center '>
-								<BtnFilledIcon variant='edit' link={`/platform/${params.projectId}/assistant/smart-assistant`} 
-								// onClick={()=> onOpen("addSmartAssistant",{projectId:projectId}
+								<BtnFilledIcon variant='edit'  
+								onClick={()=> router.push(`/platform/${params.projectId}/assistant/smart-assistant/`)
+									// onOpen("addSmartAssistant",{projectId:projectId})
 									
-								// )} 
+								} 
 								text='Умный ассистент'/>
                                 <BtnFilledIcon variant='edit' onClick={()=> onOpen("addWebPageAssistant",{projectId:projectId})} text='Асситент для работы со страницами сайта'/>
 								<BtnFilledIcon variant='edit' onClick={()=> onOpen('addAssistant',{projectId:projectId})} text='Базовый ассистент'/>
