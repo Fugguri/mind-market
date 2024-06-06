9ac6475cf4737b12a449706830b89d19184fb923
Fugguri <fygguri@gmail.com>
2024-06-06 16:01:12 +0300

    update

--- app/(main)/(routes)/platform/[projectId]/integrations/page.tsx
+++ app/(main)/(routes)/platform/[projectId]/integrations/page.tsx
@@ -1,26 +1,31 @@
-
 import { db } from "@/lib/db";
-
 import IntegrationPage from "@/components/integrations/add-integration-button";
 
+interface PageProps {
+  params: {
+    projectId: string;
+  };
+}
 
-export default  async function Page(params:{projectId:string}) {
+export default async function Page({ params }: PageProps) {
+  const projectId = params.projectId;
 
-  const projectId = params.projectId
-  
-  const assistants = await db.assistant.findMany({where: {
-    projectId
-  }})
+  const assistants = await db.assistant.findMany({
+    where: { projectId }
+  });
 
-  const telegramBots = await db.telegramBot.findMany({where: {
-    projectId:projectId
-  }})
-
-
+  const telegramBots = await db.telegramBot.findMany({
+    where: { projectId }
+  });
 
   return (
     <>
-    <IntegrationPage assistants={assistants} telegramBots={telegramBots} integrations={[]} telegramUserBots={[]}/>
+      <IntegrationPage 
+        assistants={assistants} 
+        telegramBots={telegramBots} 
+        integrations={[]} 
+        telegramUserBots={[]} 
+      />
     </>
   );
 }
